060fd92b0be4858cde2ea733d83e40fe13ee02c8
oddsan <odsan@gmail.com>
2022-01-07 20:44:15 +0100

    j2

--- 42/j02/ex00/ft_print_alphabet.c
+++ 42/j02/ex00/ft_print_alphabet.c
@@ -1,9 +1,31 @@
 #include <unistd.h>
 
-void ft_print_alphabet(void);
+int ft_putchar(char c)
+{
+    write(1, &c, 1);
+    return (0);
+}
+
+
+void ft_print_alphabet(void)
+{
+    int i; 
+    char c;
+
+    i = 0;
+    c = 'a';
+    while (i < 26)
+    {
+        ft_putchar(c);
+        i++;
+        c++;
+    }
+}
+
 
 int main()
 {
-    write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
-    return(0);
+    ft_print_alphabet();
+    ft_putchar('\n');
+    return (0);
 }
